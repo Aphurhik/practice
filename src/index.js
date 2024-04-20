ae075acb0bb1fc04a54397d3240f8ed4d6d47ba4
Aphurhi Kevwe <kevweaphurhi@gmail.com>
2024-04-20 10:39:20 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>Created by Kevwe</p>
+    <p>Copyright 2024</p>
+  </div>,
+  document.getElementById("root")
+);
+
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
